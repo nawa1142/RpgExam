a145850671d7c984b60542ba2205760ca9a9a12e
canaria <tae_ta2549@hotmail.com>
2024-04-30 18:55:26 +0700

    hide src db

--- server/plugins/dbconnection.js
+++ server/plugins/dbconnection.js
@@ -2,17 +2,25 @@ import mongoose from "mongoose"
 
 export default async () => {
   const config = useRuntimeConfig()
+
+  const dbName = config.dbName;
+  const user = config.user;
+  const pass = config.pass;
   try {
+    const connectionString = `mongodb+srv://${user}:${pass}@cluster0.fx0gmmi.mongodb.net/${dbName}?retryWrites=true&w=majority`;
+
+    await mongoose.connect(connectionString);
+    console.log("Server Connected by mongoose !");
+  /* try {
     const DB_OPTIONS = {
       dbName: config.dbName,
       user: config.user,
       pass: config.pass,
       authSource: config.authSource,
     }
-    /* await mongoose.connect(config.dburl, DB_OPTIONS) */
-    await mongoose.connect("mongodb+srv://canaria:1234@cluster0.fx0gmmi.mongodb.net/TestAuth?retryWrites=true&w=majority")
-    console.log("Server Connected by mongoose !")
+    await mongoose.connect(config.dburl, DB_OPTIONS)
+    console.log("Server Connected by mongoose !")*/
   } catch (err) {
     console.log(err)
-  }
+  } 
 }
